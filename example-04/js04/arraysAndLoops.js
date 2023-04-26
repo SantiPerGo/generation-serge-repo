c3d1957cecc0610ef87fba28e31869b698bc6ff4
Santiago Pérez Gómez <spergo@outlook.com>
2023-04-26 14:33:43 -0600

    Example04 - JS04 - Feat: Added Array Examples

---
+++ example-04/js04/arraysAndLoops.js
@@ -0,0 +1,19 @@
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// Arrays
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// Types of arrays declaration
+const names = ["Sofi", "Alejandro", "Ixchel", "Said"];
+const lastnames = new Array("Montellano", "Torres", "Vazquez");
+const cohorts = new Array(4); // Empty array with 4 spaces
+
+// Getting one element
+console.log("Sergio lastname:", lastnames[1]);
+
+// Adding one element to the array
+lastnames.push("Ruiz");
+
+// Printing arrays
+console.log(names);
+console.log(lastnames);
+console.log(cohorts);

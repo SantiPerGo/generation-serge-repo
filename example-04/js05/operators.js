849c5b155194c6b2368e9ab4a96b8b5c703e9ad4
Santiago Pérez Gómez <spergo@outlook.com>
2023-04-27 17:21:41 -0600

    Example04 - JS05 - Feat: Added Operators File

---
+++ example-04/js05/operators.js
@@ -0,0 +1,3 @@
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// Operators
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

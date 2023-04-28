eb18015b817aa54a4077ed04dfca9b4cd94f5e16
Santiago Pérez Gómez <spergo@outlook.com>
2023-04-28 11:36:30 -0600

    Example04 - JS05 - Feat: Added Operators Examples

--- example-04/js05/operators.js
+++ example-04/js05/operators.js
@@ -1,3 +1,43 @@
 // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 // Operators
 // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+/*
+    Normal (realizes operation with two numbers)
+
+    Addition +
+    Substraction -
+    Multiplication *
+    Division /
+    Exponential **
+    Remainder % 
+
+    Unirios (realizes operation with one number)
+
+    Negation - (to create negative numbers) (Ex. -200)
+    Conversion + (converts string to number) (Ex. +"200")
+
+    Logical operators
+
+    Not !
+    And &&
+    Or ||
+
+    Pre and post increment or decrement operators
+
+    Pre-increment ++x
+    Pre-decrement --x
+    Post-increment x++
+    Post-decrement x--
+
+    Short circuit operators
+
+    And && 
+        (boolean && operation) // If the boolean is true, makes the operation
+        (NaN && operation) // NaN
+        (string && operation) // Empty string is true, but with content is false
+    Or ||
+        (boolean && operation) // If the boolean is false, makes the operation
+        (NaN && operation) // Operation
+        (string && operation) // Empty string is true, but with content is false
+*/

62f5b786f30e6b24cf10a1c45f54f7424627d892
Santiago Pérez Gómez <spergo@outlook.com>
2023-05-05 11:32:13 -0600

    Example04 - JS07 - Feat: Post Petition Example

--- example-04/js07/fetch.js
+++ example-04/js07/fetch.js
@@ -19,11 +19,12 @@ const getProducts = httpUrl =>
         .catch(error => console.log(error));
 
 const url = "https://fakestoreapi.com/products";
-getProducts(url);
+// getProducts(url);
 
 // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 // Fetch API with Asyn-Await
 // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 const getProductsWithAwait = async httpUrl => {
     try {
         // HTTP get petition, returns a JSON file
@@ -33,9 +34,34 @@ const getProductsWithAwait = async httpUrl => {
         const productsObject = await productsJson.json(); 
         console.log(productsObject);
 
+        // Saving in browser
+        localStorage.setItem("my-products", JSON.stringify(productsObject));
+
         // Getting products inside object
         productsObject.forEach(product => console.log(product.title));
     } catch (error) { console.log(error); }
 };
 
-getProductsWithAwait(url);
+// getProductsWithAwait(url);
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// POST Petition
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+const postUser = (url, user) => {
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers : { 'Content-Type': 'application/json' }
+    }).then(responseJson => responseJson.json())
+      .then(responseObject => console.log(responseObject))
+      .catch(error => {});
+};
+
+const user = {
+    name: "El Santi",
+    job: "leader"
+};
+
+const urlPost = "https://reqres.in/api/users";
+postUser(urlPost, user);
